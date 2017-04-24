819279ecc83455ac5a2fbaa7b100afbc954b399e
CodeHeeler <codeheeler@gmail.com>
2017-04-24 12:42:16 -0400

    updated secret key storage and heroku config settings

--- secondchancesproject/settings.py
+++ secondchancesproject/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from .secrets import *
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
