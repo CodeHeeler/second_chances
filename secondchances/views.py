d7a5a6c70fe5c7a234df8102982d427c7a1923d9
CodeHeeler <codeheeler@gmail.com>
2017-05-05 16:32:43 -0400

    corrected passing an object when an id was needed

--- secondchances/views.py
+++ secondchances/views.py
@@ -26,10 +26,10 @@ class User_ProfileViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
 
     def get_queryset(self):
-        user_id = self.request.user.id
+        user_id = self.request.user
         user_profile = User_Profile.objects.get(user=user_id)
-        user = User.objects.get(id=self.request.user)
-        login(self.request, user)
+        user = User.objects.get(id=self.request.user.id)
+        login(self.request, user.id)
         return user_profile
 
     # def update(self, request, pk):
